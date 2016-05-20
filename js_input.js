a869f7e4979a6574f80d6589a481e746031cc9ef
Mark Harvey <markedwinharvey@gmail.com>
2016-05-20 10:13:15 -0700

    update js

--- js_input.js
+++ js_input.js
@@ -10,78 +10,77 @@ function js_input(dict){
 	//	params:		arguments to pass with function call, as a {key:value} object 
 
 	//--------------js_input_box html to append----------------------
-	var input_html = function(){/*
-	<div id='js_input_box'>
-		<div id='js_input_msg'></div>
-		<textarea id='js_input_text'></textarea>
-		<br>
-		<button class='js_input_button js_input_cancel'>cancel</button>
-		<button class='js_input_button js_input_ok'>ok</button>
+		var input_html = function(){/*
+		<div id='js_input_box'>
+			<div id='js_input_msg'>add input:</div>
+			<textarea id='js_input_text'></textarea>
+			<br>
+			<button class='js_input_button js_input_cancel'>cancel</button>
+			<button class='js_input_button js_input_ok'>ok</button>
 	
-		<style>
-			#js_input_box{
-				height:200px;
-				width:300px;
-				background-color:rgb(240,240,240);
-				border-style:solid;
-				border-color:rgb(160,160,160);
-				border-width:2px;
-				border-radius:2px;
-				text-align:center;
-				position:fixed;
-				z-index:100;
-			}
-			#js_input_text{
-				margin-top:2px;
-				height:140px;
-				width:260px;
-			}
-			#js_input_msg{
-				margin:2px;
-			}
-		</style>
-	</div>
-	*/}.toString().slice(15,-4);
+			<style>
+				#js_input_box{		
+					background-color:rgb(240,240,240);
+					border-style:solid;
+					border-color:rgb(160,160,160);
+					border-width:2px;
+					border-radius:2px;
+					text-align:center;
+					position:fixed;
+					z-index:100;
+					padding:4px;
+				}
+				#js_input_text{
+					margin:2px;
+					height:140px;
+					width:260px;
+				}
+				#js_input_msg{
+					margin:2px;
+				}
+			</style>
+		</div>
+		*/}.toString().slice(15,-4);
 	//---------------------------end html---------------------------
 	
 	//----------extract dictionary content and append html---------
-	$('*').attr('disabled','disabled');	//disable html functionality during prompt
+		$('*').attr('disabled','disabled');		//disable html on page before prompt
+
+		if(!dict.el || dict.el.length == 0){	//append prompt content
+			$('body').children().first().before(input_html);
+		}
+		else{
+			$(dict.el).append(input_html);
+		}
 	
-	if(!dict.input_msg || dict.input_msg == ''){
-		dict.input_msg='add input: ';
-	}
-	if(!dict.el || dict.el.length == 0){
-		$('body').children().first().before(input_html);
-	}
-	else{
-		$(dict.el).append(input_html);
-	}
-	$('#js_input_msg').html(dict.input_msg);	
+		if (dict.input_msg){					//add custom message if available
+			$('#js_input_msg').html(dict.input_msg);	
+		}
 	//--------------------end extract dict---------------------------
 	
 	//---------------position prompt in center of window------------
-	var margin_top =   $(window).height()/2 - $('#js_input_box').height()/2;
-	var margin_left = $(window).width()/2 - $('#js_input_box').width()/2;
-	$('#js_input_box').css({
-		'margin-left':margin_left+'px',
-		'margin-top':margin_top+'px'
-	});
+		var margin_top =   $(window).height()/2 - $('#js_input_box').height()/2;
+		var margin_left = $(window).width()/2 - $('#js_input_box').width()/2;
+		$('#js_input_box').css({
+			'margin-left':margin_left+'px',
+			'margin-top':margin_top+'px'
+		});
 	//--------------------end position prompt------------------------
 	
 	//---------------button listeners and callback-----------------------------
-	$(document).on('click','.js_input_button',function(){
-		if ( $(this).hasClass('js_input_ok') ) {
-			var user_input = $('#js_input_text').val();
-			try{
-				dict.params.user_input = user_input;	//append user_input to params
-				dict.callback(dict.params);				//callback
+		$(document).on('click','.js_input_button',function(){
+			if ( $(this).hasClass('js_input_ok') ) {
+				var user_input = $('#js_input_text').val();
+				try{
+					dict.params.user_input = user_input;	//append user_input to params
+					dict.callback(dict.params);				//callback
+				}
+				catch(err){
+					console.log('Something went wrong. Check callback and params.');
+				}
 			}
-			catch(err){
-				console.log('Something went wrong. Check callback and params.');
-			}
-		}
-		$('#js_input_box').remove();		//remove input box
-		$('*').removeAttr('disabled');		//enable all html elements
-	});
+			$('#js_input_box').remove();		//remove input box
+			$('*').removeAttr('disabled');		//enable all html elements
+		});
 	//----------------------end button listeners-------------------------
 }
